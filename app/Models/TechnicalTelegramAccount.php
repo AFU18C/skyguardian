dd5d1d7fcbb73c9568a6fccfff236c680624123e
AFU18C <bigxaos@gmail.com>
2026-07-19 20:10:05 +0300

    Detach alert sources before deleting Telegram accounts

--- app/Models/TechnicalTelegramAccount.php
+++ app/Models/TechnicalTelegramAccount.php
@@ -20,6 +20,29 @@ class TechnicalTelegramAccount extends Model
         'last_checked_at',
     ];
 
+    protected static function booted(): void
+    {
+        static::deleting(function (TechnicalTelegramAccount $account): void {
+            AlertSource::query()
+                ->where('reader_account_id', $account->getKey())
+                ->update([
+                    'reader_account_id' => null,
+                    'autopublish_enabled' => false,
+                    'source_status' => 'not_checked',
+                    'last_error' => 'Технический аккаунт чтения удалён. Выберите новый аккаунт.',
+                ]);
+
+            AlertSource::query()
+                ->where('publisher_account_id', $account->getKey())
+                ->update([
+                    'publisher_account_id' => null,
+                    'autopublish_enabled' => false,
+                    'destination_status' => 'not_checked',
+                    'last_error' => 'Технический аккаунт публикации удалён. Выберите новый аккаунт.',
+                ]);
+        });
+    }
+
     protected function casts(): array
     {
         return [
