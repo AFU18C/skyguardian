328fefe97fee821c5b1ec787c58982aadcd36d81
AFU18C <bigxaos@gmail.com>
2026-07-20 00:17:52 +0300

    Route Telegram updates to multi-group handler

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
-use App\Http\Controllers\TelegramWelcomeWebhookController;
+use App\Http\Controllers\TelegramGroupWebhookController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/telegram/welcome/{bot}', TelegramWelcomeWebhookController::class)
+Route::post('/telegram/welcome/{bot}', TelegramGroupWebhookController::class)
     ->whereIn('bot', ['news', 'alerts'])
     ->name('telegram.welcome.webhook');
