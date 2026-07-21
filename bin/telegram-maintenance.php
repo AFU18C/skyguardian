23d732a8f85d04f042c9e298682af7ac7838da92
AFU18C <bigxaos@gmail.com>
2026-07-21 18:44:07 +0300

    Add Telegram automation maintenance task

---
+++ bin/telegram-maintenance.php
@@ -0,0 +1,12 @@
+#!/usr/bin/env php
+<?php
+declare(strict_types=1);
+
+require dirname(__DIR__) . '/src/TelegramAutomation.php';
+
+try {
+    (new TelegramAutomation(dirname(__DIR__) . '/storage'))->runMaintenance();
+} catch (Throwable $exception) {
+    fwrite(STDERR, '[' . date(DATE_ATOM) . '] ' . $exception->getMessage() . PHP_EOL);
+    exit(1);
+}
