bd6e27ed00096e410d9cec75cdd9822362b0d2ff
AFU18C <bigxaos@gmail.com>
2026-07-24 12:44:25 +0300

    Redirect home through protected dashboard

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,14 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
+Route::redirect('/', '/dashboard');
+
 Route::middleware('guest')->group(function (): void {
     Route::get('/login', [AuthController::class, 'create'])->name('login');
     Route::post('/login', [AuthController::class, 'store'])->name('login.store');
 });
 
 Route::middleware('auth')->group(function (): void {
-    Route::get('/', fn () => redirect()->route('dashboard'));
-
     Route::view('/dashboard', 'dashboard')->name('dashboard');
     Route::post('/logout', [AuthController::class, 'destroy'])->name('logout');
 });
