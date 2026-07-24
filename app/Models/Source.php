3cd5409dd8491804bc7b27ba6ba2e0ca79cc276a
AFU18C <bigxaos@gmail.com>
2026-07-24 12:54:41 +0300

    Add source model

---
+++ app/Models/Source.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Source extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'type',
+        'identifier',
+        'is_active',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'is_active' => 'boolean',
+        ];
+    }
+}
