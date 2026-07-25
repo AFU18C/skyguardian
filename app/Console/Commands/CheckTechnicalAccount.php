77c7051404f2f44fca608458029348339d8d74ef
AFU18C <bigxaos@gmail.com>
2026-07-25 14:03:04 +0300

    Add technical account manual check command

---
+++ app/Console/Commands/CheckTechnicalAccount.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\TechnicalAccount;
+use App\Services\TechnicalAccountService;
+use Illuminate\Console\Command;
+use Throwable;
+
+class CheckTechnicalAccount extends Command
+{
+    protected $signature = 'skyguardian:account:check {account : ID технического аккаунта}';
+
+    protected $description = 'Выполнить ручную проверку технического Telegram-аккаунта';
+
+    public function handle(TechnicalAccountService $service): int
+    {
+        $account = TechnicalAccount::query()->findOrFail((int) $this->argument('account'));
+
+        try {
+            $service->manualCheck($account);
+            $this->info('Технический аккаунт успешно проверен.');
+            return self::SUCCESS;
+        } catch (Throwable $e) {
+            $this->error($e->getMessage());
+            return self::FAILURE;
+        }
+    }
+}
