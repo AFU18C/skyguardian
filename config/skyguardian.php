c234dcf0ab0c4a06503b954d3cd41cb89ec63b72
AFU18C <bigxaos@gmail.com>
2026-07-25 13:34:32 +0300

    Configure persistent Telethon daemon connection

--- config/skyguardian.php
+++ config/skyguardian.php
@@ -9,8 +9,8 @@ return [
         'account_concurrent_operations' => 2,
     ],
     'telethon' => [
-        'python' => env('SKYGUARDIAN_PYTHON', base_path('.venv/bin/python')),
-        'worker' => env('SKYGUARDIAN_TELETHON_WORKER', base_path('telethon/worker.py')),
+        'host' => env('SKYGUARDIAN_TELETHON_HOST', '127.0.0.1'),
+        'port' => (int) env('SKYGUARDIAN_TELETHON_PORT', 8787),
         'timeout_seconds' => (int) env('SKYGUARDIAN_TELETHON_TIMEOUT', 60),
     ],
 ];
