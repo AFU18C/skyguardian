e17827c6488ebe8b94cd5ce9d1c55470fe01e468
AFU18C <bigxaos@gmail.com>
2026-07-25 14:04:30 +0300

    Schedule SkyGuardian source processing

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,7 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+Schedule::command('skyguardian:sources:process --limit=40')
+    ->everySecond()
+    ->withoutOverlapping(1);
