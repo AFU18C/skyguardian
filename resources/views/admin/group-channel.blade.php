24634463aa6c16964f1d705d1cb4ecf2250e9298
AFU18C <bigxaos@gmail.com>
2026-07-25 21:42:41 +0300

    Добавить страницу Группа-Канал

---
+++ resources/views/admin/group-channel.blade.php
@@ -0,0 +1,12 @@
+<x-layouts.admin title="Группа-Канал">
+    <x-slot:description>Управление связками Telegram-групп и каналов.</x-slot:description>
+
+    <section class="sg-section-block">
+        <div class="sg-section-heading">
+            <div>
+                <p class="sg-eyebrow">Telegram</p>
+                <h2>Группа-Канал</h2>
+            </div>
+        </div>
+    </section>
+</x-layouts.admin>
