cdd35ef9517cd7904bfd572ba6cf1771ad17deee
AFU18C <bigxaos@gmail.com>
2026-07-25 22:30:07 +0300

    Расширить модель бота группы или канала

--- app/Models/GroupChannelBot.php
+++ app/Models/GroupChannelBot.php
@@ -10,17 +10,16 @@ class GroupChannelBot extends Model
     use HasFactory;
 
     protected $fillable = [
-        'bot_name',
-        'bot_token',
-        'admin_id',
-        'group_name',
-        'group_link',
-        'is_active',
+        'bot_name', 'bot_token', 'admin_id', 'group_name', 'group_link',
+        'chat_type', 'chat_id', 'bot_username', 'status', 'permissions',
+        'last_error', 'last_manual_check_at', 'is_active',
     ];
 
     protected $hidden = ['bot_token'];
 
     protected $attributes = [
+        'chat_type' => 'group',
+        'status' => 'not_checked',
         'is_active' => true,
     ];
 
@@ -28,6 +27,8 @@ class GroupChannelBot extends Model
     {
         return [
             'bot_token' => 'encrypted',
+            'permissions' => 'array',
+            'last_manual_check_at' => 'datetime',
             'is_active' => 'boolean',
         ];
     }
