9d6e8cb70e18387884c128b07b73c8d1ba304b2d
AFU18C <bigxaos@gmail.com>
2026-07-26 10:23:39 +0300

    Добавить настройки модулей для групп и каналов

--- app/Models/GroupChannelBot.php
+++ app/Models/GroupChannelBot.php
@@ -9,10 +9,28 @@ class GroupChannelBot extends Model
 {
     use HasFactory;
 
+    public const MODULES = [
+        'publications' => 'Публикации',
+        'drafts' => 'Черновики',
+        'scheduled_publications' => 'Отложенные публикации',
+        'auto_delete_publications' => 'Автоудаление публикаций',
+        'polls' => 'Опросы и голосования',
+        'bulk_delete' => 'Массовое удаление',
+        'antispam' => 'Антиспам',
+        'welcome' => 'Приветствие новых пользователей',
+        'subscription_check' => 'Проверка подписки',
+        'join_requests' => 'Заявки на вступление',
+        'human_verification' => 'Проверка новых участников',
+        'warnings' => 'Предупреждения и наказания',
+        'newcomer_restrictions' => 'Ограничение новичков',
+        'slow_mode' => 'Медленный режим',
+    ];
+
     protected $fillable = [
         'bot_name', 'bot_token', 'admin_id', 'group_name', 'group_link',
         'chat_type', 'chat_id', 'bot_username', 'status', 'permissions',
-        'last_error', 'last_manual_check_at', 'is_active',
+        'last_error', 'last_manual_check_at', 'is_active', 'module_settings',
+        'last_test_message_at', 'last_test_message_error',
     ];
 
     protected $hidden = ['bot_token'];
@@ -21,6 +39,7 @@ class GroupChannelBot extends Model
         'chat_type' => 'group',
         'status' => 'not_checked',
         'is_active' => true,
+        'module_settings' => '{}',
     ];
 
     protected function casts(): array
@@ -28,8 +47,15 @@ class GroupChannelBot extends Model
         return [
             'bot_token' => 'encrypted',
             'permissions' => 'array',
+            'module_settings' => 'array',
             'last_manual_check_at' => 'datetime',
+            'last_test_message_at' => 'datetime',
             'is_active' => 'boolean',
         ];
+    }
+
+    public function moduleEnabled(string $module): bool
+    {
+        return (bool) data_get($this->module_settings, $module.'.enabled', false);
     }
 }
