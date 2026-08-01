72511c60c55770284032f0244b2a3c5de16b7c24
AFU18C <bigxaos@gmail.com>
2026-08-01 12:42:14 +0300

    Подключить стили мониторинга в админке

--- resources/views/components/layouts/admin.blade.php
+++ resources/views/components/layouts/admin.blade.php
@@ -10,6 +10,7 @@
         'resources/css/source-copying.css',
         'resources/css/collapsible-cards.css',
         'resources/css/topbar-metrics.css',
+        'resources/css/source-health.css',
         'resources/css/group-channel.css',
         'resources/css/site-settings.css',
         'resources/js/app.js',
