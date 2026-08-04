fe7368e6e69430102256151d3159d1eae863c3a6
AFU18C <bigxaos@gmail.com>
2026-08-04 22:43:24 +0300

    Fix service provider import order

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
-use App\Services\GroupedAlertTelegramService;
 use App\Services\GroupChannelTelegramService;
+use App\Services\GroupedAlertTelegramService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
