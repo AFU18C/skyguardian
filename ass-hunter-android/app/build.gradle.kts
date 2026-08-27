2a40182ff350612c7b6af42a5be0644cd1fcbee3
AFU18C <bigxaos@gmail.com>
2026-08-27 08:19:31 +0300

    Add Ass Hunter app Gradle build

---
+++ ass-hunter-android/app/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.asshunter.game"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.asshunter.game"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
