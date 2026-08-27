1c52df87b74ed9fc699c7f0a5b8af034dbf2c70d
AFU18C <bigxaos@gmail.com>
2026-08-27 20:52:53 +0300

    Update Ass Hunter face edition package

--- ass-hunter-android/app/build.gradle.kts
+++ ass-hunter-android/app/build.gradle.kts
@@ -7,10 +7,10 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.asshunter.game"
+        applicationId = "com.asshunter.game.face"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
     }
 }
