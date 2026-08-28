608beccacad72ce4b1076ef2b8f591939e1861e4
AFU18C <bigxaos@gmail.com>
2026-08-28 08:39:42 +0300

    Launch clean v8 game view

--- ass-hunter-android/app/src/main/java/com/asshunter/game/MainActivity.java
+++ ass-hunter-android/app/src/main/java/com/asshunter/game/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         enterImmersive();
-        setContentView(new FastGameView(this));
+        setContentView(new GameViewV8(this));
     }
 
     @Override
