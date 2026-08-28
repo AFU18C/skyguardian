87e330bf3a93ab34b7c5fea32d4ecfcc8ccc3960
AFU18C <bigxaos@gmail.com>
2026-08-28 07:58:31 +0300

    Bump corrected build to version 6

--- ass-hunter-android/app/build.gradle.kts
+++ ass-hunter-android/app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
         applicationId = "com.asshunter.game.face"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "4.0"
+        versionCode = 6
+        versionName = "6.0"
     }
 }
