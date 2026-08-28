de5a228b18ce2302e8c269ff775687adb739f9b9
AFU18C <bigxaos@gmail.com>
2026-08-28 08:32:53 +0300

    Use fast redrawn hunter game view

--- ass-hunter-android/app/src/main/java/com/asshunter/game/MainActivity.java
+++ ass-hunter-android/app/src/main/java/com/asshunter/game/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         enterImmersive();
-        setContentView(new FixedGameView(this));
+        setContentView(new FastGameView(this));
     }
 
     @Override
