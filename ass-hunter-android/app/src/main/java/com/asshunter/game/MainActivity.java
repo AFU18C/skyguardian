491c4f574526916b42a82def2d7005e6ee650404
AFU18C <bigxaos@gmail.com>
2026-08-28 07:58:19 +0300

    Use corrected game view with all-direction shooting

--- ass-hunter-android/app/src/main/java/com/asshunter/game/MainActivity.java
+++ ass-hunter-android/app/src/main/java/com/asshunter/game/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         enterImmersive();
-        setContentView(new GameView(this));
+        setContentView(new FixedGameView(this));
     }
 
     @Override
