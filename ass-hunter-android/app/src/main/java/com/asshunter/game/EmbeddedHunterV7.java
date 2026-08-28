fae92519c80ad14f85b8e01458e141e6d9cfb529
AFU18C <bigxaos@gmail.com>
2026-08-28 08:36:10 +0300

    Remove broken hunter asset assembler

--- ass-hunter-android/app/src/main/java/com/asshunter/game/EmbeddedHunterV7.java
+++
@@ -1,8 +0,0 @@
-package com.asshunter.game;
-
-public final class EmbeddedHunterV7 {
-    private EmbeddedHunterV7() {}
-    public static String data() {
-        return HunterData1.data() + HunterData2.data() + HunterData3.data() + HunterData4.data();
-    }
-}
